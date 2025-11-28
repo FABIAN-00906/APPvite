409a8164d2da1637235ab659ebd7247e59143250
Fabian Andres Marin Montoya <fabianandres.marin01@gmail.com>
2025-11-28 11:05:58 -0500

    chore: integra StackTecnologias en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+
+import React from "react";
 import CabeceraCV from "./components/CabeceraCV.jsx";
 import Perfil from "./components/Perfil.jsx";
 import Educacion from "./components/Educacion.jsx";
 import Experiencia from "./components/Experiencia.jsx";
+import StackTecnologias from "./components/StackTecnologias.jsx";
+import "./index.css";
 
 function App() {
   return (
@@ -11,6 +15,7 @@ function App() {
         <Perfil />
         <Experiencia />
         <Educacion />
+        <StackTecnologias />
       </div>
     </div>
   );
