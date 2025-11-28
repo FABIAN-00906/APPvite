2a9f546e8ec35c77ae63affaa1e7fb79a5179ed4
Fabian Andres Marin Montoya <fabianandres.marin01@gmail.com>
2025-11-28 12:51:09 -0500

    Se modifico la foto de perfil de cabecera y el css de la foto para que se adapatara a las modificaciones

--- src/components/CabeceraCV.jsx
+++ src/components/CabeceraCV.jsx
@@ -1,7 +1,12 @@
 export default function CabeceraCV({ nombre, cargo, ciudad, contacto }) {
   return (
-    <header>
-      <img src={contacto.foto} alt="Foto" />
+    <header className="cabecera">
+      <img
+        src={contacto.foto}
+        alt="Foto de perfil"
+        className="foto-perfil"
+      />
+
       <h1>{nombre}</h1>
       <h2>{cargo}</h2>
       <p>{ciudad}</p>
